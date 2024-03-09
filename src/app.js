05b40fbeb5f1d36d62d528abd8d5fd480b61aa34
Roman Smolyakov <123468411+Roman9456@users.noreply.github.com>
2024-03-09 16:39:04 +0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const characters = [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'маг', health: 0},
+    {name: 'лучник', health: 0}
+  ];
+  
+  console.log(characters);
